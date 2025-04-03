b5ec8c51e37a6f69052aea0b13da82a7aea3e6ee
Kenley <knribnitez@gmail.com>
2025-04-04 00:06:28 +0800

    added enable edge to edge

--- app/src/main/java/com/example/gofund/MainActivity.kt
+++ app/src/main/java/com/example/gofund/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gofund
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.gofund.navigations.SetupNavGraph
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             GoFundTheme {
                 navController = rememberNavController()
